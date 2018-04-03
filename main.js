8307621e434298bc2d7e78af717d1982b91262b7
Srikanth jabbala <33954344+srikanth-jabbala2@users.noreply.github.com>
2018-04-03 16:38:52 +0530

    added code to js and json

---
+++ main.js
@@ -0,0 +1,36 @@
+function loadJSON(file,callback){
+  var xhr=new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange=function(){
+    if(xhr.readyState ===4 && xhr.status== "200"){
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
+loadJSON("data.json",function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+  career(data.career);
+  education(data.education);
+})
+var right=document.querySelector (".content");
+function career(car){
+  console.log(car.info);
+  var h3=document.createElement("h3");
+  h3.textContent="Career Objectives";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var p=document.createElement("p");
+  p.textContent=car.info;
+  right.appendChild(p);
+}
+function education(edu){
+  var h3=document.createElement("h3");
+  h3.textContent="Education";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+}
