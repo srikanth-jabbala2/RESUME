fe4658cb4d09caa109795977870bbd30420510b7
Srikanth jabbala <33954344+srikanth-jabbala2@users.noreply.github.com>
2018-04-04 12:16:12 +0530

    files added

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ loadJSON("data.json",function(text){
   console.log(data);
   career(data.career);
   education(data.education);
+  skills(data.skills);
 })
 var right=document.querySelector (".content");
 function career(car){
@@ -33,4 +34,41 @@ function education(edu){
   right.appendChild(h3);
   var hr=document.createElement("hr");
   right.appendChild(hr);
+
+  var ul=document.createElement("ul");
+  right.appendChild(ul);
+  for(var i=edu.length-1;i>=0;i--){
+    console.log(edu[i].degree);
+    var li=document.createElement("li");
+    li.textContent=edu[i].degree;
+    ul.appendChild(li);
+
+    var p=document.createElement("p");
+    p.textContent=edu[i].school;
+    li.appendChild(p);
+
+    var ul1=document.createElement("ul");
+    ul.appendChild(ul1);
+    for(j=0;j<edu[i].data.length;j++){
+      var li1=document.createElement("li");
+      li1.textContent=edu[i].data[j];
+      ul1.appendChild(li1);
+    }
+  }
+}
+function skills(skills){
+  var h3=document.createElement("h3");
+  h3.textContent="skills";
+  right.appendChild(h3);
+  var hr=document.createElement("hr");
+  right.appendChild(hr);
+  var table=document.createElement("table");
+  table.border="1";
+  right.appendChild(table);
+  let row="";
+  for(var i=0;i<skills.length;i++){
+    row+="<tr><td>"+skills[i].name+"</td><td>"+skills[i].info+"</td></tr>"
+  }
+  table.innerHTML=row;
+
 }
